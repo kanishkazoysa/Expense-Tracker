eb2e2405f1d591237d1a4ef919cbc5cfbcdd6618
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-08-30 06:17:25 +0530

    Create Db models

---
+++ ExpenseTracker/ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public string Date { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
+    }
+}
